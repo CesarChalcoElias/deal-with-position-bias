7c6597c7907de4a3d3120c0723535346b70e0e38
Cesar Chalco <cesarchalcoelias@gmail.com>
2026-04-02 11:32:37 -0500

    feat: expose IPSXGBoostAdapter and config classes in public API

---
+++ unbiastap/__init__.py
@@ -0,0 +1,11 @@
+from unbiastap.models.pointwise_clf.ips_xgboost import IPSXGBoostAdapter
+from unbiastap.models.pointwise_clf.config import (
+    IPSXGBoostConfig,
+    IPSXGBoostHyperparams
+)
+
+__all__ = [
+    "IPSXGBoost",
+    "IPSXGBoostConfig",
+    "IPSXGBoostHyperparams"
+]
